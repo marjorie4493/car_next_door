d43c473368e25cbca08ac7473ff8362eea3e8c94
marjorie4493 <marjorie4493@hotmail.com>
2013-10-02 12:26:58 +1000

    Revision 93

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,21 @@
 module SessionsHelper
+
+  def randomise
+    random = []
+  
+    while random.length != 3 do
+      number = rand(1..16)
+  	  included = false
+      random.each do |n|
+        if n == number
+	      included = true
+	    end
+	  end
+      if included == false
+        random << number
+      end	  
+    end  
+	random
+  end
+  
 end
